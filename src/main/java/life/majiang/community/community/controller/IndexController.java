8f439afcae18d27a80210f0c48163a5cf5800ffb
xiaowenhan12138 <972024456@qq.com>
2019-08-09 09:49:46 +0800

    h2

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,16 +1,12 @@
 package life.majiang.community.community.controller;
 
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
         return "index";
     }
 }
